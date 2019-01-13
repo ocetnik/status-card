7bf48a690207d8f373f46b6e1490ced308c7c7a8
Dávid Ocetník <ocetnik.david@gmail.com>
2019-01-13 19:52:47 +0100

    Updates

---
+++ precache-manifest.433e91b3cd39ca5e3a0220b6f768f35d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aafbd8cdf9098fd956f5",
+    "url": "/status-card/static/css/main.e9ad6537.chunk.css"
+  },
+  {
+    "revision": "aafbd8cdf9098fd956f5",
+    "url": "/status-card/static/js/main.aafbd8cd.chunk.js"
+  },
+  {
+    "revision": "229f9f0dddfa342cf5e8",
+    "url": "/status-card/static/js/1.229f9f0d.chunk.js"
+  },
+  {
+    "revision": "512be5bf45200f4ecedb",
+    "url": "/status-card/static/js/runtime~main.512be5bf.js"
+  },
+  {
+    "revision": "44891b0db6f72291644bf6f4eb2ba9a6",
+    "url": "/status-card/index.html"
+  }
+];
